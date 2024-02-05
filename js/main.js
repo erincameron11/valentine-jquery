9a1e18304b8264e8794417d1f91943b2f35726f7
Erin Cameron <erincameron11@hotmail.com>
2024-02-04 21:58:28 -0500

    work on No button movement

--- js/main.js
+++ js/main.js
@@ -1,30 +1,45 @@
 // jQuery
 $(document).ready(function() {
+    // TEST
+    // console.log($("#title").offset().left, $("#title").offset().top);
+    var rect = $("#title").getBoundingClientRect;
+    console.log(rect.top, rect.right, rect.bottom, rect.left); 
+
     // Event listener for No button click
-    $(".no-btn").click(function() {
+    $(".no").click(function() {
         alert("yay");
     });
 
     // Event listener for No button cursor hover
-    $(".no-btn").mouseenter(function() {
-        var move = randomDirection();
-        var moveHorizontal = move[0];
-        var moveVertical = move[1];
+    $(".no").mouseenter(function() {
+        var moveHorizontal = randomHorizontal();
+        var moveVertical = randomVertical();
+        // var moveVertical = "0px";
         $(this).animate({left: moveHorizontal, top: moveVertical});
     });
 
     // Event listener for Yes button click
-    $(".heart-btn").click(function() {
+    $(".yes").click(function() {
         // Hide the No button when the Yes button is clicked
-        $(".no-btn").hide();
+        $(".no").hide();
     });
 })
 
 
-// Function to move the No button on hover
-function randomDirection() {
+// FUNCTION: used to move the No button horizontally on hover
+function randomHorizontal() {
+    var boundary = window.innerWidth - $(".no").offset().left;
     var randHorizontal = Math.floor(Math.random() * (window.innerWidth - 100));
+    console.log("window:  " + window.innerWidth);
+    console.log("boundary:  " + boundary);
+    console.log("horizontal: " + randHorizontal);
+    return boundary + "px";
+}
+
+
+// FUNCTION: used to move the No button horizontally on hover
+function randomVertical() {
     var randVertical = Math.floor(Math.random() * (window.innerHeight - 100));
-    // console.log([randX, randY]);
-    return randHorizontal + "px", randVertical + "px";
+    // console.log("vertical: " + randVertical);
+    return randVertical + "px";
 }
